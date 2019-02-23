6820cc459864116682b017b9518658e687adef06
Anton Pedersen <antonpedersen@Antons-MacBook-Pro.local>
2019-02-23 15:15:41 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Carrus AB</title>
+    <meta name="description" content="Carrus AB">
+    <meta name="author" content="OAS">
+
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <div class="main-wrapper">
+
+        <div class="content">
+            
+        </div>
+        
+    </main>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
